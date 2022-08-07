cf0fd8b73c0580cb158861e984159147ce75b69c
mudassir619 <mudassirsiddiqui27@gmail.com>
2022-08-07 17:29:44 +0500

    styling fixes

--- src/pages/CarNavigation.jsx
+++ src/pages/CarNavigation.jsx
@@ -6,10 +6,10 @@ export default function CarNavigation() {
   return (
     <div>
         <div className="mapComponent grid grid-cols-12 w-[100%] h-100vh">
-            <div className="carNavigationSidebar md:col-span-3 sm:hidden">
+            <div className="carNavigationSidebar md:col-span-3 hidden">
                 car is ...
             </div>
-            <div className="carNavigationmapComponent sm:col-span-12 md:col-span-9 w-[100%] h-[100vh]">
+            <div className="carNavigationmapComponent col-span-12 md:col-span-9 w-[100%] h-[100vh]">
                 <CarNavigationmapComponent />
             </div>
         </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,10 @@ function Dashboard({socket}) {
   const [locationAdded,setLocationAdded] = useState()
   return (
     <div className="grid grid-cols-12 dashboard main w-[100%] h-[100vh]">
-      <div className="sidebar sm:hidden md:col-span-3">
+      <div className="sidebar hidden md:col-span-3">
         <div className="text-3xl w-[100%] text-center my-2">Sidebar</div>
       </div>
-      <div className="mapContainer sm:col-span-12 md:col-span-9 w-[100%] h-[100%]">
+      <div className="mapContainer col-span-12 md:col-span-9 w-[100%] h-[100%]">
         <MapComponent socket={socket} locationAdded={locationAdded} setLocationAdded={setLocationAdded} />
       </div>
     </div>
